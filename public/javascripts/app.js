d989120b335903f4942c77652689437a5c77a6d7
Gavin Nitta <gshigeto@gmail.com>
2017-03-16 10:05:14 -0600

    Show winners and losers images on click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,18 +1,30 @@
 angular.module('Creative4', ['ngMaterial'])
   .controller('MainCtrl', [
-    '$scope', '$http', '$timeout',
-    function ($scope, $http, $timeout) {
+    '$scope', '$http', '$timeout', '$mdDialog',
+    function ($scope, $http, $timeout, $mdDialog) {
 
       $timeout(function () {
         $scope.showHint = true;
       }, 5000);
 
       $scope.getImages = function () {
-        $http.get(`/images?text=${$scope.winner}`).then(resp => {
-          console.log(resp.data.images);
+
+        if ($scope.winner == null || $scope.loser == null) {
+          return $mdDialog.show(
+            $mdDialog.alert()
+              .clickOutsideToClose(true)
+              .title('Whoa, hold on there!')
+              .textContent('Please fill in the winning and losing teams!')
+              .ariaLabel('Alert Dialog')
+              .ok('Okay!')
+          );
+        }
+
+        $http.get(`/images?text=${$scope.winner} basketball`).then(resp => {
+          $scope.winners = resp.data.images;
         });
-        $http.get(`/images?text=${$scope.loser}`).then(resp => {
-          console.log(resp.data.images);
+        $http.get(`/images?text=${$scope.loser} basketball`).then(resp => {
+          $scope.losers = resp.data.images;
         });
       };
     }
